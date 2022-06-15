3b0e3f4a0e10949692cff8a793e5267583b14ba4
Kostya Gunya <31970225+Ginyola@users.noreply.github.com>
2022-06-15 21:32:28 +0300

    изменён файл

--- combined.js
+++ combined.js
@@ -4,8 +4,9 @@ import { check } from 'k6';
 //Base config
 export const options = {
   scenarios: {
-    base_scenario: {
+    ya: {
       executor: 'ramping-arrival-rate',
+      exec: 'yaRu',
       startRate: 0,
       timeUnit: '1m',
       preAllocatedVUs: 1,
@@ -17,6 +18,21 @@ export const options = {
         { target: 72, duration: '5m' },
         { target: 72, duration: '10m' }
       ]
+    },
+    www: {
+      executor: 'ramping-arrival-rate',
+      exec: 'wwwRu',
+      startRate: 0,
+      timeUnit: '1m',
+      preAllocatedVUs: 1,
+      maxVUs: 10,
+
+      stages: [
+        { target: 120, duration: '5m' },
+        { target: 120, duration: '10m' },
+        { target: 144, duration: '5m' },
+        { target: 144, duration: '10m' }
+      ]
     }
   },
 
@@ -26,8 +42,15 @@ export const options = {
   }
 }
 
-export default function () {
+export function yaRu () {
   const res = http.get('http://ya.ru');
+  check(res, {
+    'Response status 200': (r) => r.status === 200
+  });
+}
+
+export function wwwRu () {
+  const res = http.get('http://www.ru');
   check(res, {
     'Response status 200': (r) => r.status === 200
   });
